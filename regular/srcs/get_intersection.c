2367f9c8d35e801d9801f74a3afb29d16895cd0e
Thibault Panou De Faymoreau <tpanou-d@1D3.42angouleme.fr>
2026-05-29 14:50:20 +0200

    simplified var names

--- regular/srcs/get_intersection.c
+++ regular/srcs/get_intersection.c
@@ -6,27 +6,27 @@
 /*   By: tpanou-d <tpanou-d@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/05/27 17:08:58 by tpanou-d          #+#    #+#             */
-/*   Updated: 2026/05/29 14:49:33 by tpanou-d         ###   ########.fr       */
+/*   Updated: 2026/05/29 14:50:16 by tpanou-d         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <math.h>
 #include "../includes/shapes.h"
 
-bool	get_plane_intersection(t_plane *pl, t_ray *ray, t_vector *res)
+bool	get_plane_intersection(t_plane *pl, t_ray *r, t_vector *res)
 {
 	t_vector	tmp;
 	float		t;
 
-	if (!vector_dot_product(&ray->n, &pl->n))
+	if (!vector_dot_product(&r->n, &pl->n))
 		return (false);
-	t = -vector_dot_product(&ray->o, &pl->n)
-		/ vector_dot_product(&ray->n, &pl->n);
-	*res = point_on_ray(ray, t);
+	t = -vector_dot_product(&r->o, &pl->n)
+		/ vector_dot_product(&r->n, &pl->n);
+	*res = point_on_ray(r, t);
 	return (true);
 }
 
-bool	get_sphere_intersection(t_sphere *sp, t_ray *ray,
+bool	get_sphere_intersection(t_sphere *sp, t_ray *r,
 	t_vector *res)
 {
 	float	d;
