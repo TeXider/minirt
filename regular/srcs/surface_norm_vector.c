0e15f4b5f30f9bb42ac48b161fc924988b1edab1
TeXider <texider.code@gmail.com>
2026-06-02 14:16:48 +0200

    created file containing surface norm vectors funcs

---
+++ regular/srcs/surface_norm_vector.c
@@ -0,0 +1,22 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   surface_norm_vector.c                              :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: almighty <almighty@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2026/06/02 12:28:23 by almighty          #+#    #+#             */
+/*   Updated: 2026/06/02 12:36:17 by almighty         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "../includes/shapes.h"
+
+t_vector	sphere_surface_norm_vector(t_sphere *sp, t_vector *p)
+{
+	t_vector	v;
+
+	v = vector_sub(&sp->o, p);
+	vector_normalize(&v);
+	return (v);
+}
