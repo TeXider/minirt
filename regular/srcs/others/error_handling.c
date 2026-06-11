18f52486c41bc48940ab1c4fcf48b3750f7002c8
TeXider <texider.code@gmail.com>
2026-06-11 21:43:41 +0200

    more precise err contextualization for debugging

--- regular/srcs/others/error_handling.c
+++ regular/srcs/others/error_handling.c
@@ -6,7 +6,7 @@
 /*   By: almighty <almighty@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/06/08 21:01:38 by almighty          #+#    #+#             */
-/*   Updated: 2026/06/08 23:12:29 by almighty         ###   ########.fr       */
+/*   Updated: 2026/06/11 20:47:26 by almighty         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,7 +14,7 @@
 
 void	print_parsing_error(t_parsing *p)
 {
-	printf("Ouch: %d, \"%s\" -> %zu:'%c'\n", p->parsing_err, p->line, p->curr_field_i, p->line[p->line_i]);
+	printf("Ouch: %d (%s), \"%s\" -> %zu:'%s', %zu:'%s'\n", p->parsing_err, p->curr_field_name, p->line, p->curr_field_i, p->line + p->curr_field_i, p->line_i, p->line + p->line_i);
 }
 
 void	print_error(t_env *env)
