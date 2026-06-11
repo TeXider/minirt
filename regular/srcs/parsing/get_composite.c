3ef8422b77dfd4bf4d350613bd37a3a49502cf20
TeXider <texider.code@gmail.com>
2026-06-11 20:10:42 +0200

    get composite field functions implemented

---
+++ regular/srcs/parsing/get_composite.c
@@ -0,0 +1,37 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_composite.c                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: almighty <almighty@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2026/06/11 20:02:50 by almighty          #+#    #+#             */
+/*   Updated: 2026/06/11 20:08:53 by almighty         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "../../includes/parsing.h"
+
+bool	get_vector(t_vector *dst, float range[2], t_parsing *p)
+{
+	if (get_float(&dst->x, range, true, p)
+		|| get_float(&dst->y, range, true, p)
+		|| get_float(&dst->z, range, false, p))
+	{
+		p->parsing_err = INVALID_FIELD_ERR;
+		return (true);
+	}
+	return (false);
+}
+
+bool	get_color(t_color *dst, t_parsing *p)
+{
+	if (get_int(&dst->r, (int[2]){0, 255}, true, p)
+		|| get_float(&dst->g, (int[2]){0, 255}, true, p)
+		|| get_float(&dst->b, (int[2]){0, 255}, false, p))
+	{
+		p->parsing_err = INVALID_FIELD_ERR;
+		return (true);
+	}
+	return (false);
+}
