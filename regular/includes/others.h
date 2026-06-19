59103f46c6d8c7b72ca560b9ba888f0689381829
TeXider <texider.code@gmail.com>
2026-06-19 12:09:02 +0200

    moved t_pol_coef to rt_math.h

--- regular/includes/others.h
+++ regular/includes/others.h
@@ -6,7 +6,7 @@
 /*   By: almighty <almighty@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/05/27 15:47:13 by tpanou-d          #+#    #+#             */
-/*   Updated: 2026/06/11 21:38:21 by almighty         ###   ########.fr       */
+/*   Updated: 2026/06/19 11:26:40 by almighty         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -37,13 +37,6 @@ typedef struct s_color
 	int	g;
 	int	b;
 }	t_color;
-
-typedef struct s_pol_coef
-{
-	float	a;
-	float	b;
-	float	c;
-}	t_pol_coef;
 
 bool	extend_planes_len(t_env *env);
 bool	extend_spheres_len(t_env *env);
