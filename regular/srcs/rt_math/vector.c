19301a5bb7ef9d80eedeb90c380a5019dff7ec64
TeXider <texider.code@gmail.com>
2026-06-19 12:03:11 +0200

    changed by ref args to by copy args in vector computation because apparently its more easily optimized by the compiler

--- regular/srcs/rt_math/vector.c
+++ regular/srcs/rt_math/vector.c
@@ -6,36 +6,35 @@
 /*   By: almighty <almighty@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/05/27 15:27:56 by tpanou-d          #+#    #+#             */
-/*   Updated: 2026/06/08 21:20:33 by almighty         ###   ########.fr       */
+/*   Updated: 2026/06/15 16:12:37 by almighty         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <math.h>
 #include "../../includes/env.h"
 
-inline void	init_vector(t_vector *v, float x, float y, float z)
+inline t_vector	vector_scale(t_vector v, float k)
 {
-	v->x = x;
-	v->y = y;
-	v->z = z;
+	return ((t_vector){k * v.x, k * v.y, k * v.z});
 }
 
-inline t_vector	vector_scale(t_vector *v, float k)
+inline t_vector	vector_add(t_vector v, t_vector u)
 {
-	return ((t_vector){k * v->x, k * v->y, k * v->z});
+	return ((t_vector){v.x + u.x, v.y + u.y, v.z + u.z});
 }
 
-inline t_vector	vector_add(t_vector *v, t_vector *u)
+inline t_vector	vector_sub(t_vector v, t_vector u)
 {
-	return ((t_vector){v->x + u->x, v->y + u->y, v->z + u->z});
+	return ((t_vector){v.x - u.x, v.y - u.y, v.z - u.z});
 }
 
-inline t_vector	vector_sub(t_vector *v, t_vector *u)
+inline t_vector	vector_normalize(t_vector v)
 {
-	return ((t_vector){v->x - u->x, v->y - u->y, v->z - u->z});
-}
+	float	norm;
 
-inline float	vector_dot_prod(t_vector *v, t_vector *u)
-{
-	return (v->x * u->x + v->y * u->y + v->z * u->z);
+	norm = vector_norm(v);
+	if (!norm)
+		return ((t_vector){1, 0, 0});
+	else
+		return (vector_scale(v, 1.0f / norm));
 }
--- regular/srcs/rt_math/vector2.c
+++ regular/srcs/rt_math/vector2.c
@@ -6,29 +6,23 @@
 /*   By: almighty <almighty@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/05/27 15:27:56 by tpanou-d          #+#    #+#             */
-/*   Updated: 2026/06/11 12:55:10 by almighty         ###   ########.fr       */
+/*   Updated: 2026/06/19 11:47:47 by almighty         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../../includes/env.h"
 
-inline float	vector_square(t_vector *v)
+inline float	vector_dot_prod(t_vector v, t_vector u)
+{
+	return (v.x * u.x + v.y * u.y + v.z * u.z);
+}
+
+inline float	vector_square(t_vector v)
 {
 	return (vector_dot_prod(v, v));
 }
 
-inline float	vector_norm(t_vector *v)
+inline float	vector_norm(t_vector v)
 {
 	return (sqrt(vector_square(v)));
-}
-
-inline void	vector_normalize(t_vector *v)
-{
-	float	norm;
-
-	norm = vector_norm(v);
-	if (!norm)
-		*v = (t_vector) {1, 0, 0};
-	else
-		*v = vector_scale(v, 1 / vector_norm(v));
 }
