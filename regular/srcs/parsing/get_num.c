0cb85de4b2be56d7bc27385d8e08694bf5d6a599
TeXider <texider.code@gmail.com>
2026-06-30 10:52:21 +0200

    fixed p->line_i condition error + get_float() sign is now only set at the very end

--- regular/srcs/parsing/get_num.c
+++ regular/srcs/parsing/get_num.c
@@ -6,7 +6,7 @@
 /*   By: almighty <almighty@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/06/07 21:09:13 by almighty          #+#    #+#             */
-/*   Updated: 2026/06/11 20:06:31 by almighty         ###   ########.fr       */
+/*   Updated: 2026/06/30 09:46:29 by almighty         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -39,7 +39,7 @@ static bool	get_mantissa(float *dst, float range[2], t_parsing *p)
 		dec /= 10.0;
 		p->line_i++;
 	}
-	*dst += mantissa * (1.0 - 2.0 * ((*((int *)(dst)) & (1 << 31)) != 0));
+	*dst += mantissa;
 	return (check_range(*dst, range[MIN], range[MAX], p));
 }
 
@@ -52,14 +52,13 @@ bool	get_float(float *dst, float range[2], bool comma_expected, t_parsing *p)
 	p->line_i += (sgn || p->line[p->line_i] == '+');
 	start_i = p->line_i;
 	*dst = 0;
-	while (p->line[p->line_i] >= '0' && p->line_i <= '9')
+	while (p->line[p->line_i] >= '0' && p->line[p->line_i] <= '9')
 	{
 		*dst = *dst * 10.0 + p->line[p->line_i] - '0';
 		if (check_range(*dst * (1.0 - 2.0 * sgn), range[MIN], range[MAX], p))
 			return (true);
 		p->line_i++;
 	}
-	*((int *)(dst)) |= (sgn << 31);
 	if (!is_end_of_field(p) && get_mantissa(dst, range, p))
 		return (true);
 	if (start_i == p->line_i || (!is_end_of_field(p)
@@ -68,6 +67,7 @@ bool	get_float(float *dst, float range[2], bool comma_expected, t_parsing *p)
 		p->parsing_err = INVALID_FIELD_ERR;
 		return (true);
 	}
+	*dst *= 1.0 - 2.0 * sgn;
 	p->line_i += comma_expected;
 	return (false);
 }
@@ -84,7 +84,7 @@ bool	get_int(int *dst, int range[2], bool comma_expected, t_parsing *p)
 		return (true);
 	}
 	*dst = 0;
-	while (p->line[p->line_i] >= '0' && p->line_i <= '9')
+	while (p->line[p->line_i] >= '0' && p->line[p->line_i] <= '9')
 	{
 		*dst = *dst * 10 + p->line[p->line_i] - '0';
 		if (check_range(*dst * (1 - 2 * sgn), range[MIN], range[MAX], p))
